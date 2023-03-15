597e89a3daeee830805bc5a4d6ae1a6e16961f83
Ankit Kumar <ankit969kumar@gmail.com>
2023-03-15 15:48:13 +0530

    my

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "this" {
+  ami                     = "ami-0f8ca728008ff5af4"
+  instance_type           = "t2.micro"
+  count                   = 2
+  tags                    = {
+        name = "Terraform"
+}
+
+}
+/*resource "aws_s3_bucket" "b" {
+  bucket = "ankit-first-bucket-hulululululu"
+
+  tags = {
+    Name        = "My bucket"
+  }
+}*/
+
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    access_key = "AKIAWZHX2BEMUMFX3RXK"
+    secret_key = "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"
+    region = "ap-south-1"
+}
+
